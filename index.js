1d1bed6d9b790c9cfbe1c429fa9a7454ca5ec54d
titus <titusmoyo86@gmail.com>
2019-05-27 10:39:49 +0100

    chenged the message Object

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app=express();
 
 
 app.get('/',(req,res)=>{
-  res.send({hi:'there..l love you all'});
+  var msgObject={
+    from:"Titus Moyo",
+    senderId:"gyftyghuji87678iuygtfxdftgh",
+    text:"Dude are you gonna participate in the Call for Code 2019",
+    time:"2 hours ago",
+  }
+  res.send(msgObject);
 });
 
 const PORT=process.env.PORT || 5000;
